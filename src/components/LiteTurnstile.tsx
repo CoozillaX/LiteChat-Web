66a6538b5c647f3b0d374877443d6b9386f719f1
CoozillaX <79683806+CoozillaX@users.noreply.github.com>
2025-12-06 23:24:52 +1100

    add skeleton for LiteTurnstile

--- src/components/LiteTurnstile.tsx
+++ src/components/LiteTurnstile.tsx
@@ -1,9 +1,16 @@
-import { forwardRef, useContext, useImperativeHandle, useRef } from "react";
+import {
+  forwardRef,
+  useContext,
+  useImperativeHandle,
+  useRef,
+  useState
+} from "react";
 import { ColorModeContext } from "@/views/App";
 import { useTranslation } from "react-i18next";
 import Turnstile from "react-turnstile";
 import type { TurnstileProps, BoundTurnstileObject } from "react-turnstile";
 import type { SupportedLang } from "@/i18n";
+import { Box, Skeleton } from "@mui/material";
 
 export interface LiteTurnstileRef {
   reset: () => void;
@@ -17,6 +24,9 @@ const LiteTurnstile = forwardRef<LiteTurnstileRef, LiteTurnstileProps>(
     const { i18n } = useTranslation();
     const boundRef = useRef<BoundTurnstileObject | null>(null);
 
+    // Loading state
+    const [isLoaded, setIsLoaded] = useState(false);
+
     // Expose reset method to parent components
     useImperativeHandle(ref, () => ({
       reset() {
@@ -28,18 +38,36 @@ const LiteTurnstile = forwardRef<LiteTurnstileRef, LiteTurnstileProps>(
     const { onLoad, ...rest } = props;
 
     return (
-      <Turnstile
-        sitekey={import.meta.env.VITE_TURNSTILE_SITE_KEY}
-        theme={ctx?.mode === "dark" ? "dark" : "light"}
-        language={i18n.language as SupportedLang}
-        size="flexible"
-        appearance="always"
-        onLoad={(widgetId, bound) => {
-          boundRef.current = bound;
-          onLoad?.(widgetId, bound);
-        }}
-        {...rest}
-      />
+      <Box sx={{ position: "relative", width: 1, minHeight: 65 }}>
+        {!isLoaded && (
+          <Skeleton
+            variant="rectangular"
+            animation="wave"
+            sx={{
+              position: "absolute",
+              inset: 0,
+              borderRadius: 1,
+              width: 1,
+              height: 1
+            }}
+          />
+        )}
+
+        <Turnstile
+          sitekey={import.meta.env.VITE_TURNSTILE_SITE_KEY}
+          theme={ctx?.mode === "dark" ? "dark" : "light"}
+          language={i18n.language as SupportedLang}
+          size="flexible"
+          appearance="always"
+          fixedSize
+          onLoad={(widgetId, bound) => {
+            boundRef.current = bound;
+            onLoad?.(widgetId, bound);
+            setIsLoaded(true);
+          }}
+          {...rest}
+        />
+      </Box>
     );
   }
 );
